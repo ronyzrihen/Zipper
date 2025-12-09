f22ac3c8b8fb0cf6220676b5460f09599d265c4c
ronyzrihen <ronyzn3@gmail.com>
2025-12-09 22:35:19 +0200

    Fix: added file and zipped folders are now being created if not already existed

--- job/zip_job.py
+++ job/zip_job.py
@@ -8,6 +8,8 @@ VERSION = os.getenv("VERSION")
 def create_files():
     for file in file_names:
         try:
+            if not os.path.exists(FILE_PATH):
+                os.makedirs(FILE_PATH)
             with open(os.path.join(FILE_PATH, f'{file}.txt'), 'w') as f:
                 pass
         except FileNotFoundError:
@@ -16,6 +18,8 @@ def create_files():
 def zip_files():
     for file in file_names:
         try:
+            if not os.path.exists(ZIP_PATH):
+                os.makedirs(ZIP_PATH)
             new_zip_name = os.path.join(ZIP_PATH, f'{file}_{VERSION}.zip')
             full_file_path = os.path.join(FILE_PATH, f'{file}.txt')
             command = f"zip {new_zip_name} {full_file_path}"
